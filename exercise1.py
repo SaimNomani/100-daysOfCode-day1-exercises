e912453431adbcee8610d2dcbd88c38ea7d7464e
SaimNomani <nomanisaim591@gmail.com>
2023-09-27 05:18:15 +0500

    I have completed exercise 2, which involved debugging.

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,6 @@
-# Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the Python print function.
-# Example Output
-# After you have written your code, you should run your program and it should print the following:
+# Write a program in main.py that prints the same notes from the previous lesson using what you have learnt about the
+# Python print function. Example Output After you have written your code, you should run your program and it should
+# print the following:
 #
 # Day 1 - Python Print Function
 # The function is declared like this:
