6d4894ad089aa03b793233243367dab2d4d53a70
SaimNomani <nomanisaim591@gmail.com>
2023-09-27 05:56:18 +0500

    exercise 3: input function
    I have completed exercise 3, which involved the use of the input function in python.

---
+++ exercise3.py
@@ -0,0 +1,6 @@
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that
+# calculates the length of a string.
+# Example Input: Angela
+# Example Output: 6
+print(len(input("what is your name: ")))
+
